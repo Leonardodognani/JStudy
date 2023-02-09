b65854426024e0ef2cd5e501bbb48d47edb3eb4f
Leonardo Dognani <roberto.leonardo@venturus.org.br>
2023-02-09 16:29:42 -0300

    exercicio com NodeList resolvido

--- Aula40-Modelo copy/assets/js/main.js
+++ Aula40-Modelo copy/assets/js/main.js
@@ -1,1 +1,14 @@
+const paragrafos = document.querySelector(".paragrafos");
+const ps = paragrafos.querySelectorAll("p"); // aqui ele seleciona todos os 'p', com índice, como array.
 
+// colocar a cor de fundo dos paragrafos da mesma do backgroundColorBody e a cor dos textos dos 'p' em branco.
+
+const estilosBody = getComputedStyle(document.body);
+const estilosContainer = getComputedStyle(document.querySelector(".container"));
+const backgroundColorContainer = estilosContainer.backgroundColor;
+const backgroundColorBody = estilosBody.backgroundColor;
+
+for (let p of ps) {
+  p.style.backgroundColor = backgroundColorBody;
+  p.style.color = backgroundColorContainer;
+}
