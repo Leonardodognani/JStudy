7888bee6ab7eda8737845daf6959f9dbef2c1874
Leonardo Dognani <roberto.leonardo@venturus.org.br>
2023-02-14 14:46:42 -0300

    exercicio do timer

--- Aula49-exercicio/assets/js/main.js
+++ Aula49-exercicio/assets/js/main.js
@@ -1,18 +1,45 @@
-const relogio = document.querySelector(".relogio");
-const iniciar = document.querySelector(".iniciar");
-const pausar = document.querySelector(".pausar");
-const zerar = document.querySelector(".zerar");
+function relogio() {
+  const relogio = document.querySelector(".relogio");
+  const iniciar = document.querySelector(".iniciar");
+  const pausar = document.querySelector(".pausar");
+  const zerar = document.querySelector(".zerar");
 
-// lembrete pra quando pegar amanhã pra continuar: primeiro tem que por o preventDefault, pros cliques não reiniciarem a página.
+  function criaHoraDosSegundos(segundos) {
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString("pt-BR", { hour12: false, timeZone: "GMT" });
+  }
 
-iniciar.addEventListener("click", function (event) {
-  alert("clicado no iniciar!");
-});
+  let segundos = 0;
+  let timer;
 
-pausar.addEventListener("click", function (event) {
-  alert("clicado no pausar!");
-});
+  function iniciaRelogio() {
+    timer = setInterval(function () {
+      segundos++;
+      relogio.innerHTML = criaHoraDosSegundos(segundos);
+    }, 1000);
+  }
 
-zerar.addEventListener("click", function (event) {
-  alert("clicado no zerar!");
-});
+  document.addEventListener("click", function (e) {
+    const el = e.target;
+
+    if (el.classList.contains("zerar")) {
+      clearInterval(timer);
+      relogio.innerHTML = "00:00:00";
+      segundos = 0;
+      relogio.classList.remove("pausado");
+    }
+
+    if (el.classList.contains("pausar")) {
+      clearInterval(timer);
+      relogio.classList.add("pausado");
+    }
+
+    if (el.classList.contains("iniciar")) {
+      relogio.classList.remove("pausado");
+      clearInterval(timer);
+      iniciaRelogio();
+    }
+  });
+}
+
+relogio();
